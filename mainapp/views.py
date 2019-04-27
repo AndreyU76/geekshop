9338eabc17e53692da54351db86b427354d39af3
AndreyU1976 <uab2@mail.ru>
2019-04-27 18:05:00 +0300

    77755

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 
 def main(request):
-    return render(request, 'mainapp/index.html')
+    context = {'name':  'Андрей'}
+    return render(request, 'mainapp/index.html', context)
 
 def products(request):
     return render(request, 'mainapp/products.html')
 
 def contacts(request):
     return render(request, 'mainapp/contacts.html')
+
